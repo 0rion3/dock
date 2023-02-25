104ff9bc57ccb9f4486c7ca29fdcee48b92642ed
orion3 <noreply@orion3>
2023-02-25 09:21:33 +0700

    Add `dcu` alias to only show currently running containers (as in "container Up")

--- hostscripts/docker_aliases.sh
+++ hostscripts/docker_aliases.sh
@@ -3,6 +3,7 @@
 alias di="docker images --format \"table {{.ID}}\t{{.Repository}}:{{.Tag}}\t{{.Size}}\t{{.CreatedSince}}\""
 alias dri="docker rmi"
 alias dc="docker ps -a --format \"table {{.ID}}\t{{.Image}}\t{{.Names}}\t{{.Status}}\""
+alias du="docker ps -a --filter='status=running' --format \"table {{.ID}}\t{{.Image}}\t{{.Names}}\t{{.Status}}\""
 alias di-size="docker ps -a --format \"table {{.ID}}\t{{.Image}}\t{{.Names}}\t{{.Size}}\t{{.Status}}\""
 alias dcr="docker rm -f"
 alias dcs="docker container stop"
